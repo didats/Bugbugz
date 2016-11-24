a01a34dae705c06e8f0d5233d934a4e265a2d0cf
Didats Triadi <didats@gmail.com>
2016-11-24 23:12:08 +0700

    Adding database SQL File

---
+++ bugs.sql
@@ -0,0 +1,162 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.13)
+# Database: bugs
+# Generation Time: 2016-11-24 16:11:26 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table attachments
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `attachments`;
+
+CREATE TABLE `attachments` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(100) DEFAULT NULL,
+  `issue_id` varchar(100) DEFAULT NULL,
+  `attachment_id` varchar(100) DEFAULT NULL,
+  `attachment_name` varchar(100) DEFAULT NULL,
+  `attachment_file` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `key_index` (`user_id`,`issue_id`,`attachment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table comments
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `comment_id` varchar(100) DEFAULT NULL,
+  `user_id` varchar(100) DEFAULT NULL,
+  `issue_id` varchar(100) DEFAULT NULL,
+  `comment_text` text,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table issues
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `issues`;
+
+CREATE TABLE `issues` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `issue_id` varchar(100) DEFAULT NULL,
+  `project_id` varchar(100) DEFAULT NULL,
+  `user_id` varchar(100) DEFAULT NULL,
+  `issue_title` varchar(255) DEFAULT NULL,
+  `issue_desc` text,
+  `issue_reporter` varchar(100) DEFAULT NULL,
+  `issue_type` varchar(50) DEFAULT NULL COMMENT 'BUG, NEW FEATURE, IMPROVEMENT',
+  `issue_priority` varchar(50) DEFAULT NULL COMMENT 'MINOR, MAJOR, BLOCKER, CRITICAL',
+  `issue_status` varchar(50) DEFAULT NULL COMMENT 'OPEN, REVIEW, FAILED, DONE',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `key_index` (`issue_id`,`project_id`,`user_id`,`issue_type`,`issue_priority`,`issue_status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table projects
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `projects`;
+
+CREATE TABLE `projects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `project_id` varchar(100) DEFAULT NULL,
+  `project_name` varchar(255) DEFAULT NULL,
+  `project_description` text,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `key_index` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table userdevices
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `userdevices`;
+
+CREATE TABLE `userdevices` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(100) DEFAULT NULL,
+  `device_id` varchar(100) DEFAULT NULL,
+  `device_token` varchar(255) DEFAULT NULL,
+  `device_os` varchar(30) DEFAULT NULL COMMENT 'MACOS, IOS, ANDROID',
+  `device_model` varchar(100) DEFAULT NULL,
+  `device_timezone` varchar(100) DEFAULT NULL,
+  `device_osversion` varchar(30) DEFAULT NULL,
+  `device_appversion` varchar(30) DEFAULT NULL,
+  `user_agent` text,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `key_index` (`user_id`,`device_id`,`device_token`,`device_os`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(100) DEFAULT NULL,
+  `user_email` varchar(100) DEFAULT NULL,
+  `user_name` varchar(100) DEFAULT NULL,
+  `user_password` varchar(255) DEFAULT NULL,
+  `user_fullname` varchar(255) DEFAULT NULL,
+  `user_type` varchar(50) DEFAULT NULL COMMENT 'ADMIN, DEVELOPER, TESTER',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_lastloggedin` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `key_index` (`user_id`,`user_email`,`user_type`,`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `user_id`, `user_email`, `user_name`, `user_password`, `user_fullname`, `user_type`, `created_at`, `user_lastloggedin`)
+VALUES
+	(1,'111-111','admin@someemail.com','admin','06f1fe4ee081458d93c8526cd7059507','Admin','ADMIN','2016-11-24 23:11:09',NULL);
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
